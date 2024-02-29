b21875f269191f7163d3491a2b91688489cb6af0
NTTrung9204 <trung9204@gmail.com>
2024-02-29 23:56:22 +0700

    complete tictactoe app

--- ReactApp/src/JsComponents/TicTacToe/TicTacToe.js
+++ ReactApp/src/JsComponents/TicTacToe/TicTacToe.js
@@ -2,15 +2,47 @@ import './Square.js';
 import Square from './Square.js';
 import { useState } from 'react';
 
+function checkWinner(squares) {
+    console.log(squares);
+    const lines = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
+
 function TicTacToe() {
     const [isTurnX, setIsTurnX] = useState(true);
+    const [squares, setSquares] = useState(Array(9).fill(null));
 
     function handleClick(event) {
-        const index = event.target.getAttribute('Square_id')
-        document.querySelector(`[ChildSquare_id="${index}"]`).classList.add('Cross');
+        try{
+            const index = event.target.getAttribute('Square_id')
+            const label = isTurnX ? 'Cross' : 'Circle';
+            const newSquares = squares.map((square, i) => i === parseInt(index) ? label : square)
+
+            document.querySelector(`[ChildSquare_id="${index}"]`).classList.add(label);
+            
+            const winner = checkWinner(newSquares);
+            if(winner) {
+                alert(`${winner} wins!`);
+            }
+            
+            setSquares(newSquares);
+            setIsTurnX(!isTurnX);
+        }
+        catch{
+            console.log("Can't fill this blank space!");
+        }
     }
 
-    const squares = Array(9).fill(null);
     return (
         <div className="TicTacToe">
             {
