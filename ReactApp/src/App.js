93e01b587b1f6d8989834290ed97ec627f2c41cb
NTTrung9204 <trung9204@gmail.com>
2024-02-29 14:47:47 +0700

    Todo list app

--- ReactApp/src/App.js
+++ ReactApp/src/App.js
@@ -1,27 +1,14 @@
 import './App.css';
 import { useState } from 'react';
-import './Toggle.css';
-
-function Button({content, onClick}){
-  return <button onClick = {onClick}>{content}</button>
-}
-
+import './Css/Toggle.css';
+import './JsComponents/BackGround.js';
+import BackGround from './JsComponents/BackGround.js';
 
 
 function App() {
-  const [state, setState] = useState(false);
-
-  const callBack = ()=>{
-    setState(state => !state)
-  }
 
   return (
-    <div className="App" style={{margin: '50px'}}>
-      <div className={`Toggle ${state? "Switch" : ""}`}>
-        <div className={`Spin ${state? "Switch" : ""}`} onClick = {callBack}></div>
-      </div>
-      <p>{state? "ON" : "OFF"}</p>
-    </div>
+      <BackGround />
   );
 }
 
---
+++ ReactApp/src/JsComponents/BackGround.js
@@ -0,0 +1,13 @@
+import '../Css/Static.css';
+import './TodoApp.js';
+import TodoApp from './TodoApp.js';
+
+function BackGround() {
+  return (
+    <div className="backGround">
+        <TodoApp />
+    </div>
+    );
+}
+
+export default BackGround;
